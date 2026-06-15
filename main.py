54d1e9bef7f6fda7b655768a6241686d2302c3cf
shubham-alyan24 <shubhamalyan1411@gmail.com>
2026-06-15 12:42:05 +0530

    add schemas and create post endpoint; resolve merge conflicts in snippets

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate , PostResponse
 
 posts: list[dict] = [
     {
@@ -29,7 +30,6 @@ app.mount("/static",StaticFiles(directory="static"),
 
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/",include_in_schema=False, name = "home")
 @app.get("/posts",include_in_schema=False, name = "posts")
 def home(request: Request):
@@ -52,11 +52,11 @@ def post_page(request:Request ,post_id : int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND
                         ,detail="post not found")
 
-@app.get("/api/posts")
+@app.get("/api/posts",response_model=list[PostResponse])
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}",response_model=PostResponse)
 def get_post(post_id : int):
     for post in posts:
         if post.get("id") == post_id:
@@ -64,6 +64,22 @@ def get_post(post_id : int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND
                         ,detail="post not found")
 
+@app.post(
+    "/api/posts",
+    response_model=PostResponse ,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1 
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "June 14, 2026"
+    }
+    posts.append(new_post)
+    return new_post
 
 @app.exception_handler(StarletteHTTPException)
 def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
